92d79ba8db99ccd10d99bc247ae2f809c0783c69
EdonFGashi <edongashi2004@outlook.com>
2024-05-02 12:11:31 +0200

    Folderat për strukturimin e kodit

---
+++ src/main/java/app/FirstApp.java
@@ -0,0 +1,12 @@
+package app;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class FirstApp extends Application {
+
+    @Override
+    public void start(Stage stage){
+        stage.show();
+    }
+}
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,7 @@ module com.example.knk_project_gr10_2024 {
     requires javafx.controls;
     requires javafx.fxml;
 
+    opens app to javafx.graphics;
 
     opens com.example.knk_project_gr10_2024 to javafx.fxml;
     exports com.example.knk_project_gr10_2024;
