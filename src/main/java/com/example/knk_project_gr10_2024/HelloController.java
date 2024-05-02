eeab1bf005f515b921fd7c058092155586ab6d6e
EdonFGashi <edongashi2004@outlook.com>
2024-05-02 12:04:02 +0200

    Initial commit

---
+++ src/main/java/com/example/knk_project_gr10_2024/HelloController.java
@@ -0,0 +1,14 @@
+package com.example.knk_project_gr10_2024;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.example.knk_project_gr10_2024 {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.example.knk_project_gr10_2024 to javafx.fxml;
+    exports com.example.knk_project_gr10_2024;
+}
