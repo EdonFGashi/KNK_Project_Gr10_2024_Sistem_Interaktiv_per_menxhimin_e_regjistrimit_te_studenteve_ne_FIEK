2cd1ffcf8b2ef73f4628184c2695a5cae2b05ecd
Dren Morina <drenmorina321@gmail.com>
2024-05-13 19:57:06 +0200

    Ndreqe foton ndashboard

--- src/main/java/controller/Overall/DashboardController.java
+++ src/main/java/controller/Overall/DashboardController.java
@@ -3,8 +3,26 @@ package controller.Overall;
 import app.Navigatior;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 
 public class DashboardController {
+
+    @FXML
+    private ImageView mainImage;
+
+    @FXML
+    private void initialize(){
+        try {
+            this.mainImage.setImage(new Image(new FileInputStream("Images/rektorati.jpg")));
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
     @FXML
     private void handleQasja(ActionEvent ae){
 
