28d7f5e8a4299b281f6ac89639e08c2928651e80
Dren Morina <drenmorina321@gmail.com>
2024-05-13 17:32:21 +0200

    Rifraktoroje kodin ne LoginService

--- src/main/java/service/Overall/LoginService.java
+++ src/main/java/service/Overall/LoginService.java
@@ -8,61 +8,81 @@ import repository.Supervisor.SupervisorRepository;
 import service.PasswordHasher;
 
 public class LoginService {
+
+    public static final String ADMIN_EMAIL_DOMAIN = "@admin.uni-pr.edu";
+    public static final String SUPERVISOR_EMAIL_DOMAIN = "@uni-pr.edu";
+    public static final String STUDENT_EMAIL_DOMAIN = "@student.uni-pr.edu";
+
+
     public static boolean login(LoginDto loginDto){
         String email = loginDto.getUserEmail();
-        if (!email.contains("@")){
+        if (!isValidEmail(email)){
             System.out.println("Nuk eshte email valid!");
             return false;
         }
 
         String emailDomain = email.substring(email.indexOf("@"));
 
-        if (emailDomain.equals("@admin.uni-pr.edu")){
-//            Kodi per admin
-            System.out.println("Eshte admin");
-
-            Admin admin = AdminRepository.getByEmail(loginDto.getUserEmail());
-            if (admin == null){
-                System.out.println("Admini nuk u gjend ne databaze");
-                return false;
-            } else {
-                System.out.println("Admini u gjend!");
+        switch (emailDomain) {
+            case ADMIN_EMAIL_DOMAIN -> {
+                return loginAsAdmin(loginDto);
             }
-
-        } else if (emailDomain.equals("@uni-pr.edu")){
-//            Kodi per mbikqyres
-            System.out.println("Eshte mbikqyres");
-
-            SupervisorTableModel supervisor = SupervisorRepository.getSupervisorByEmail(loginDto.getUserEmail());
-            if (supervisor == null){
-                System.out.println("Mbikqyresi nuk u gjend ne databaze");
-                return false;
+            case SUPERVISOR_EMAIL_DOMAIN -> {
+                return loginAsSupervisor(loginDto);
             }
-
-            System.out.println("Mbikqyresi u gjend!");
-
-            String password = loginDto.getUserPassword();
-            String salt = supervisor.getSalt();
-            String passwordHash = supervisor.getPasswordHash();
-
-            return PasswordHasher.compareSaltedHash(
-                    password, salt, passwordHash
-            );
-
-
-        } else if (emailDomain.equals("@student.uni-pr.edu")){
-//            Kodi per student
-            System.out.println("Eshte student");
-
-
-
-
-        } else {
-            System.out.println("Domeni nuk i perket fakultetit!");
+            case STUDENT_EMAIL_DOMAIN -> {
+                return loginAsStudent(loginDto);
+            }
+            default -> {
+//
+                System.out.println("Domeni nuk i perket fakultetit!");
+            }
         }
 
-        System.out.println(email.substring(email.indexOf("@")));
+        return false;
+    }
 
+
+    private static boolean isValidEmail(String email){
+        return email.contains("@");
+    }
+
+    private static boolean loginAsAdmin(LoginDto loginDto){
+        System.out.println("Eshte admin");
+
+        Admin admin = AdminRepository.getByEmail(loginDto.getUserEmail());
+        if (admin == null){
+            
+            System.out.println("Admini nuk u gjend ne databaze");
+            return false;
+        }
+
+        System.out.println("Admini u gjend!");
+        return true;
+    }
+
+    private static boolean loginAsSupervisor(LoginDto loginDto){
+        System.out.println("Eshte mbikqyres");
+
+        SupervisorTableModel supervisor = SupervisorRepository.getSupervisorByEmail(loginDto.getUserEmail());
+        if (supervisor == null){
+            System.out.println("Mbikqyresi nuk u gjend ne databaze");
+            return false;
+        }
+
+        System.out.println("Mbikqyresi u gjend!");
+
+        String password = loginDto.getUserPassword();
+        String salt = supervisor.getSalt();
+        String passwordHash = supervisor.getPasswordHash();
+
+        return PasswordHasher.compareSaltedHash(
+                password, salt, passwordHash
+        );
+    }
+
+    private static boolean loginAsStudent(LoginDto loginDto){
+        System.out.println("Eshte student");
         return false;
     }
 }
