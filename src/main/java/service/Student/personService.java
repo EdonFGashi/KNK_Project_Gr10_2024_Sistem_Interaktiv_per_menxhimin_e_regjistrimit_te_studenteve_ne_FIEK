13d025dfa4a2312d2553fc3dc6e48c1b0c43771c
Erza <erzagashi04@gmail.com>
2024-05-16 18:09:41 +0200

    Krijimi i PersonService

---
+++ src/main/java/service/Student/personService.java
@@ -0,0 +1,27 @@
+package service.Student;
+
+
+
+
+import model.dto.Student.PersonDTO;
+import repository.StudentApplicant.StudentApplicantRepository;
+import service.DBConnector;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class personService {
+
+
+
+    // Metoda për të marrë personin nga bazat e të dhënave duke përdorur numrin personal
+    public static PersonDTO getPersonByPersonalNumber(String personalNumber) {
+
+  PersonDTO person=  StudentApplicantRepository.SearchByPersonalNumber(personalNumber);
+
+        return person;
+    }
+}
