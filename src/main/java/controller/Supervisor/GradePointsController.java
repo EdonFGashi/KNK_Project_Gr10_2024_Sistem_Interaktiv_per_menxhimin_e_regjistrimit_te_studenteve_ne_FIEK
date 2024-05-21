2f8da0024ef3eec444af11ba8930b5591a8d58b6
Dren Morina <drenmorina321@gmail.com>
2024-05-21 17:41:18 +0200

    Fshije kodin qe nuk vyn te GradePointsController

--- src/main/java/controller/Supervisor/GradePointsController.java
+++ src/main/java/controller/Supervisor/GradePointsController.java
@@ -63,22 +63,14 @@ public class GradePointsController {
     private Afat selectedAfat;
     @FXML
     private void initialize(){
-//        choiseChoseLevel.setItems(FXCollections.observableArrayList("Bachelor", "Master", "Doctorature"));
 
         try {
             this.imgSearch.setImage(new Image(new FileInputStream("Images/search.png")));
         } catch (FileNotFoundException e) {
             System.out.println("Image not found");
         }
-//        tableKonkurimet.getSelectionModel().selectedItemProperty().addListener((obs, oldSelection, newSelection) -> {
-//            if (newSelection != null) {
-//                this.selectedKonkurim = newSelection;
-//                this.setTextFields();
-//            }
-//        });
 
         tableKonkurimet.getSelectionModel().selectedItemProperty().addListener((obs, oldSelection, newSelection) -> {
-            System.out.println("SelectedItemProperty: " + tableKonkurimet.getSelectionModel().selectedItemProperty().get().getPiket());
             if (newSelection != null) {
                 this.selectedKonkurim = newSelection;
                 int mbikqyresiId = newSelection.getMbikqyresiId();
@@ -91,14 +83,6 @@ public class GradePointsController {
                 newSelection.setAplikimiId(mbikqyresiId);
                 newSelection.setPiket(aplikimiId);
 
-
-
-                System.out.println();
-                System.out.println("The new selection: ");
-                System.out.println("MbikqyresiId: " + mbikqyresiId);
-                System.out.println("StudentiId: " + studentiId);
-                System.out.println("AplikimiId: " + aplikimiId);
-                System.out.println("Piket: " + piket);
                 this.setTextFields();
             }
         });
@@ -148,18 +132,10 @@ public class GradePointsController {
 //        Ktu ruhen konkurimet e kerkuara
 //        this.konkurimetList = SupervisorService.searchKonkurimi(this.txtSearch.getText().trim());
         this.selectedKonkurim = SupervisorService.searchKonkurimi(this.txtSearch.getText().trim());
-        System.out.println("--------------------------");
-        System.out.println("konkurimetList: " + this.selectedKonkurim);
-        System.out.println("The new selection: ");
-        System.out.println("MbikqyresiId: " + this.selectedKonkurim.getMbikqyresiId());
-        System.out.println("StudentiId: " + this.selectedKonkurim.getStudentiId());
-        System.out.println("AplikimiId: " + this.selectedKonkurim.getAplikimiId());
-        System.out.println("Piket: " + this.selectedKonkurim.getPiket());
-        System.out.println("--------------------------");
+
         setTextFields();
         this.konkurimetList = SupervisorService.searchKonkurimet(this.txtSearch.getText().trim());
 //        SESSION.setAdmin_supervisor_lastSearch(this.txtSearch.getText().trim());
-//        this.setColumns();
     }
     @FXML
     private void handleSearchClick(MouseEvent me){
@@ -167,34 +143,16 @@ public class GradePointsController {
         setTextFields();
         this.konkurimetList = SupervisorService.searchKonkurimet(this.txtSearch.getText());
 //        SESSION.setAdmin_supervisor_lastSearch(this.txtSearch.getText().trim());
-//        this.setColumns();
     }
-
-    @FXML
-    private void handleDelete(ActionEvent ae){
-//        if(SupervisorRepository.deleteSupervisor(Integer.parseInt(this.supervisorId.getText()))){
-//            PopUp.tick(150);
-////            this.konkurimetList = SupervisorService.searchMbikqyresi(SESSION.getAdmin_supervisor_lastSearch());
-//            this.setColumns();
-//    }else{
-//        System.out.println("Nuk u fshi");
-//    }
-    }
-
 
     private void setColumns(){
         this.columnMbikqyresi.setCellValueFactory(new PropertyValueFactory<>("piket"));
         this.columnStudenti.setCellValueFactory(new PropertyValueFactory<>("studentiId"));
         this.columnAplikimi.setCellValueFactory(new PropertyValueFactory<>("mbikqyresiId"));
         this.columnPiket.setCellValueFactory(new PropertyValueFactory<>("aplikimiId"));
-        this.tableKonkurimet.setItems(this.konkurimetList); }
+        this.tableKonkurimet.setItems(this.konkurimetList);
+    }
 
-//    private void setTextFields(){
-//        this.supervisorId.setText(Integer.toString(this.selectedSupervisor.getMbikqyresiId()));
-//        this.studentId.setText(this.selectedSupervisor.getEmail());
-//        this.applicationId.setText(this.selectedSupervisor.getFirstName());
-//        this.piket.setText(this.selectedSupervisor.getLastName());
-//        }
 
     private void setTextFields(){
         this.supervisorId.setText(Integer.toString(this.selectedKonkurim.getMbikqyresiId()));
@@ -208,9 +166,6 @@ public class GradePointsController {
     }
     private void disableForms() {
         this.piket.setEditable(false);
-//        this.choiseChoseLevel.setDisable(true);
-//        this.dateOpenDate.setDisable(true);
-//        this.dateClosedDate.setDisable(true);
     }
 
 
