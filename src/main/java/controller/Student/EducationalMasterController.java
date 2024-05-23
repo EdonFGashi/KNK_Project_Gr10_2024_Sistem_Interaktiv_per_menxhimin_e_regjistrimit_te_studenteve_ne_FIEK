7e02a9f315d9fefc6d7fd886f1bd5054273c462b
KaltrinaKrasniqi <kaltrina.krasniqi31@student.uni-pr.edu>
2024-05-23 20:48:05 +0200

    shtimi i sample master controller

---
+++ src/main/java/controller/Student/EducationalMasterController.java
@@ -0,0 +1,64 @@
+package controller.Student;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+
+public class EducationalMasterController {
+
+    @FXML
+    private AnchorPane addPane;
+
+    @FXML
+    private Button btnChooseFile1;
+
+    @FXML
+    private Button btnChooseFile2;
+
+    @FXML
+    private ImageView imgBachelorDegree;
+
+    @FXML
+    private ImageView imgIdeentification;
+
+    @FXML
+    private TextField txtDept;
+
+    @FXML
+    private TextField txtFaculty;
+
+    @FXML
+    private TextField txtFirstYear;
+
+    @FXML
+    private TextField txtSecYear;
+
+    @FXML
+    private TextField txtThirdYear;
+
+    @FXML
+    void handleBack(ActionEvent event) {
+
+    }
+
+    @FXML
+    void handleContinue(ActionEvent event) {
+
+    }
+
+    @FXML
+    void handleFile1(ActionEvent event) {
+
+    }
+
+    @FXML
+    void handleFile2(ActionEvent event) {
+
+    }
+
+
+}
