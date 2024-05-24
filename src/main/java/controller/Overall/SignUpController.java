647e0b300de196d44ea7b5a75d6cef79dbd5f182
Dren Morina <drenmorina321@gmail.com>
2024-05-24 22:55:34 +0200

    Shto validimin te SignUp

--- src/main/java/controller/Overall/SignUpController.java
+++ src/main/java/controller/Overall/SignUpController.java
@@ -4,12 +4,15 @@ import app.Navigatior;
 import controller.SESSION;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.util.Duration;
 import model.dto.Overall.UserDto;
+import service.CustomExceptions.InvalidEmail;
+import service.CustomExceptions.InvalidPassword;
 import service.Student.UserService;
 
 import java.io.FileInputStream;
@@ -34,6 +37,11 @@ public class SignUpController {
     @FXML
     private PasswordField pwdPassword;
 
+    @FXML
+    private Label errorMessageLabel;
+
+
+
     private Locale currentLocale = new Locale("en");
     @FXML
     private void handleChangeLanguage(ActionEvent ae){
@@ -46,22 +54,34 @@ public class SignUpController {
     }
 
     @FXML
-    private void handleSignUp(ActionEvent ae) throws SQLException {
-        UserDto userSignUpData = new UserDto(
-                this.txtUsername.getText(),
-                this.txtEmail.getText(),
-                this.pwdPassword.getText(),
-                this.pwdConfirmPassword.getText()
-        );
+    private void handleSignUp(ActionEvent ae) throws SQLException, InvalidEmail, InvalidPassword {
 
-        boolean response = UserService.signUp(userSignUpData);
+        errorMessageLabel.setText("");
+        errorMessageLabel.setVisible(false);
 
-        if(response){
-            SESSION.setLoggedUser(UserService.getUserByEmail(this.txtEmail.getText()));
-          System.out.println("Okej");
-            Navigatior.navigateNewStage(Navigatior.STUDENT_DASHBOARD);
-            Navigatior.closeStageAfterDelay(ae, Duration.millis(1));
+        try {
+            UserDto userSignUpData = new UserDto(
+                    this.txtUsername.getText(),
+                    this.txtEmail.getText(),
+                    this.pwdPassword.getText(),
+                    this.pwdConfirmPassword.getText()
+            );
+            System.out.println("Bout to try signup");
+            boolean response = UserService.signUp(userSignUpData);
+            System.out.println("Sign up was succesful");
+
+            if(response){
+                SESSION.setLoggedUser(UserService.getUserByEmail(this.txtEmail.getText()));
+                System.out.println("Okej");
+                Navigatior.navigateNewStage(Navigatior.STUDENT_DASHBOARD);
+                Navigatior.closeStageAfterDelay(ae, Duration.millis(1));
+            }
+        } catch (SQLException | InvalidEmail | InvalidPassword e){
+            System.out.println("Inside catch at SignUpController");
+            errorMessageLabel.setText(e.getMessage());
+            errorMessageLabel.setVisible(true);
         }
+
 
     }
 
--- src/main/java/service/Student/UserService.java
+++ src/main/java/service/Student/UserService.java
@@ -13,20 +13,36 @@ import model.dto.Student.UserProfileDto;
 import repository.AdminRepository;
 import repository.StudentApplicant.StudentApplicantRepository;
 import repository.UserRepository;
+import service.CustomExceptions.InvalidEmail;
+import service.CustomExceptions.InvalidPassword;
 import service.PasswordHasher;
 
 import java.sql.SQLException;
 import java.util.List;
 
 public class UserService {
-    public static boolean signUp(UserDto userData) throws SQLException {
+    public static boolean signUp(UserDto userData) throws SQLException, InvalidPassword, InvalidEmail {
         System.out.println("Te service");
         String password = userData.getPassword();
         String confirmPassword = userData.getConfirmPassword();
+        String email = userData.getEmail();
 
-        if (!password.equals(confirmPassword)) {
-            return false;
+
+        if (!isValidEmail(email)){
+            System.out.println("Incorrect email format");
+            throw new InvalidEmail("Incorrect email format");
         }
+        System.out.println("Email is valid");
+        if (password.length() < 8){
+            throw new InvalidPassword("Password too short");
+        }
+
+        System.out.println("Password length is good");
+        if (!password.equals(confirmPassword)) {
+            throw new InvalidPassword("Passwords do not match");
+        }
+
+        System.out.println("Passwords do match");
 
         String salt = PasswordHasher.generateSalt();
         String passwordHash = PasswordHasher.generateSaltedHash(
@@ -44,6 +60,11 @@ public class UserService {
     }
 
 
+    private static boolean isValidEmail(String email){
+        String emailPattern = "^.+@.+\\..+$";
+        // Kontrollon nese emaili i pershtatet patternit
+        return email.matches(emailPattern);
+    }
     
     public static User getUserByEmail(String email){
         return UserRepository.getByEmail(email);
