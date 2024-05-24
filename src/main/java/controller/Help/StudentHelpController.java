1472590096c4a4f173046cba6a9c52175e5b9f64
Endrit <endritkastrati8@gmail.com>
2024-05-24 17:52:40 +0200

    Adding translation in help-information pages.

--- src/main/java/controller/Help/StudentHelpController.java
+++ src/main/java/controller/Help/StudentHelpController.java
@@ -1,15 +1,28 @@
 package controller.Help;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.util.Locale;
 
 public class StudentHelpController {
     @FXML
     private ImageView helpLogo;
+
+    private Locale currentLocale = new Locale("en");
+    @FXML
+    private void handleChangeLanguage(ActionEvent ae){
+        if (currentLocale.getLanguage().equals("en")) {
+            currentLocale = new Locale("sq");
+        } else {
+            currentLocale = new Locale("en");
+        }
+        //loadLanguage(currentLocale.getLanguage());
+    }
 
     @FXML
     private void initialize(){
