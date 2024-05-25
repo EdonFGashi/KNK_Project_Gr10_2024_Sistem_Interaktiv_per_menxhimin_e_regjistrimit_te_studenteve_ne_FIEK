3ea1407118e12054ba50ca919b042c84afce85fa
KaltrinaKrasniqi <kaltrina.krasniqi31@student.uni-pr.edu>
2024-05-25 03:40:39 +0200

    mbledhja pikeve ne menyre dinamike te educational-experience

--- src/main/java/controller/Student/EducationalExperienceController.java
+++ src/main/java/controller/Student/EducationalExperienceController.java
@@ -2,6 +2,8 @@ package controller.Student;
 
 import app.Navigatior;
 import controller.SESSION;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
@@ -14,9 +16,7 @@ import model.dto.Student.StudentApplicantDto;
 
 import java.io.FileInputStream;
 import java.io.IOException;
-
 import java.io.File;
-import static controller.SESSION.getLoggedUser;
 
 import static controller.SESSION.getLoggedUser;
 
@@ -26,55 +26,45 @@ public class EducationalExperienceController {
     private AnchorPane addpane;
     @FXML
     private Button chooseImageButton1;
-
     @FXML
     private Button chooseImageButton2;
-
     @FXML
     private Button chooseImageButton3;
-
     @FXML
     private ImageView imgDiplome;
-
     @FXML
     private ImageView imgGradeCertificate;
-
     @FXML
     private ImageView imgIdentification;
-
     @FXML
     private TextField txtAlbanian;
-
     @FXML
     private TextField txtAllPoints;
-
     @FXML
     private TextField txtChosenSubject;
-
     @FXML
     private TextField txtChosenSubjectPoints;
-
     @FXML
     private TextField txtEnglish;
-
     @FXML
     private TextField txtGrade10;
-
     @FXML
     private TextField txtGrade11;
-
     @FXML
     private TextField txtGrade12;
-
     @FXML
     private TextField txtMath;
-
     @FXML
     private TextField txtSchoolName;
 
     public File imageFile1;
     public File imageFile2;
     public File imageFile3;
+
+    @FXML
+    void initialize() {
+        addTextFieldListeners();
+    }
 
     @FXML
     void handleBack(ActionEvent event) {
@@ -84,7 +74,7 @@ public class EducationalExperienceController {
     @FXML
     void handleContinue(ActionEvent event) {
         String ShkMesme = txtSchoolName.getText();
-        int Matematike =Integer.parseInt(txtMath.getText());
+        int Matematike = Integer.parseInt(txtMath.getText());
         int Shqip = Integer.parseInt(txtAlbanian.getText());
         int Anglisht = Integer.parseInt(txtEnglish.getText());
         String LendaZgjedhur = txtChosenSubject.getText();
@@ -94,35 +84,31 @@ public class EducationalExperienceController {
         Double Sukses11 = Double.parseDouble(txtGrade11.getText());
         Double Sukses12 = Double.parseDouble(txtGrade12.getText());
 
-        // e shtove SESSION para getLoggedUser()
-        StudentApplicantDto dto = new StudentApplicantDto(SESSION.getLoggedUser().getId(), ShkMesme, Matematike, Shqip, Anglisht, LendaZgjedhur, PikatLendesZgjedhur,PikatGjithsej,Sukses10,Sukses11,Sukses12,imageFile1,imageFile2,imageFile3);
+
+        StudentApplicantDto dto = new StudentApplicantDto(SESSION.getLoggedUser().getId(), ShkMesme, Matematike, Shqip, Anglisht, LendaZgjedhur, PikatLendesZgjedhur, PikatGjithsej, Sukses10, Sukses11, Sukses12, imageFile1, imageFile2, imageFile3);
 
         try {
             service.Student.StudentApplicantService.processAndSaveData(dto);
-            // Trego një mesazh suksesi
+
         } catch (Exception e) {
             e.printStackTrace();
-            // Trego një mesazh gabimi
-        }
-        Navigatior.navigate(addpane,Navigatior.ACADEMIC);
 
+        }
+        Navigatior.navigate(addpane, Navigatior.ACADEMIC);
     }
 
     @FXML
     void handleFile1(ActionEvent event) {
         imageFile1 = chooseImage(imgGradeCertificate);
-
     }
 
     @FXML
     void handleFile2(ActionEvent event) {
-
         imageFile2 = chooseImage(imgIdentification);
     }
 
     @FXML
     void handleFile3(ActionEvent event) {
-
         imageFile3 = chooseImage(imgDiplome);
     }
 
@@ -143,4 +129,27 @@ public class EducationalExperienceController {
         return selectedFile;
     }
 
+    private void addTextFieldListeners() {
+        ChangeListener<String> changeListener = (observable, oldValue, newValue) -> calculateTotalPoints();
+
+        txtAlbanian.textProperty().addListener(changeListener);
+        txtEnglish.textProperty().addListener(changeListener);
+        txtMath.textProperty().addListener(changeListener);
+        txtChosenSubjectPoints.textProperty().addListener(changeListener);
+    }
+
+    private void calculateTotalPoints() {
+        try {
+            int albanianPoints = Integer.parseInt(txtAlbanian.getText());
+            int englishPoints = Integer.parseInt(txtEnglish.getText());
+            int mathPoints = Integer.parseInt(txtMath.getText());
+            int chosenSubjectPoints = Integer.parseInt(txtChosenSubjectPoints.getText());
+
+            int totalPoints = albanianPoints + englishPoints + mathPoints + chosenSubjectPoints;
+            txtAllPoints.setText(String.valueOf(totalPoints));
+        } catch (NumberFormatException e) {
+
+            txtAllPoints.setText("");
+        }
+    }
 }
